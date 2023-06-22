be9c6a95bd596f68d98f7bcfab0d56b7d13941f3
Matthew Pickett <beak765@gmail.com>
2023-06-22 14:05:04 -0700

    Refactor code; fix minor bugs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,8 +38,11 @@ namespace Pacman_Game
         private Path Ghost2;
         private Path Ghost3;
         private Path Ghost4;
-        private List<UIElement> separateFoods = new List<UIElement>();
-
+        private bool isGhost1 = false;
+        private bool isGhost2 = false;
+        private bool isGhost3 = false;
+        private bool isGhost4 = false;
+        private List<UIElement> bigFoods = new List<UIElement>();
         private DrawPacmanImage PacmanImage = new DrawPacmanImage();
         private DispatcherTimer gameTimer = new System.Windows.Threading.DispatcherTimer();
         private DispatcherTimer ghostTimer = new System.Windows.Threading.DispatcherTimer();
@@ -69,8 +72,6 @@ namespace Pacman_Game
 
         private void RestartGame()
         {
-            if (fruit != null)
-                GameArea.Children.Remove(fruit);
             GameArea.Children.Remove(PacmanImage.UiElement);
 
             pacmanDirection = Direction.Right;
@@ -108,27 +109,31 @@ namespace Pacman_Game
             GameArea.Children.Add(Ghost1);
             Canvas.SetTop(Ghost1, squareSize * 13);
             Canvas.SetLeft(Ghost1, squareSize * 8);
+            isGhost1 = true;
 
             Ghost2 = DrawGhost.CreateGhost(Brushes.Cyan);
             GameArea.Children.Add(Ghost2);
             Canvas.SetTop(Ghost2, squareSize * 8);
             Canvas.SetLeft(Ghost2, squareSize * 20);
+            isGhost2 = true;
 
             Ghost3 = DrawGhost.CreateGhost(Brushes.Pink);
             GameArea.Children.Add(Ghost3);
             Canvas.SetTop(Ghost3, squareSize * 13);
             Canvas.SetLeft(Ghost3, squareSize * 10);
+            isGhost3 = true;
 
             Ghost4 = DrawGhost.CreateGhost(Brushes.Yellow);
             GameArea.Children.Add(Ghost4);
             Canvas.SetTop(Ghost4, squareSize * 11);
             Canvas.SetLeft(Ghost4, squareSize * 17);
+            isGhost4 = true;
         }
         private void DrawGameArea()
         {
             // Clear existing elements from the game area
             GameArea.Children.Clear();
-            separateFoods.Clear();
+            bigFoods.Clear();
 
             int maxX = (int)(GameArea.ActualWidth / squareSize);
             int maxY = (int)(GameArea.ActualHeight / squareSize);
@@ -154,16 +159,16 @@ namespace Pacman_Game
                     if (squareNumber == 26 || squareNumber == 46 || squareNumber == 223 || squareNumber == 350 || squareNumber == 374)
                     {
                         // Add a separate food item to the current square
-                        UIElement separateFood = new Ellipse()
+                        UIElement bigFood = new Ellipse()
                         {
                             Width = squareSize * 0.75,
                             Height = squareSize * 0.75,
                             Fill = Brushes.Tan
                         };
-                        GameArea.Children.Add(separateFood);
-                        Canvas.SetTop(separateFood, y * squareSize + squareSize * 0.25);
-                        Canvas.SetLeft(separateFood, x * squareSize + squareSize * 0.25);
-                        separateFoods.Add(separateFood);
+                        GameArea.Children.Add(bigFood);
+                        Canvas.SetTop(bigFood, y * squareSize + squareSize * 0.25);
+                        Canvas.SetLeft(bigFood, x * squareSize + squareSize * 0.25);
+                        bigFoods.Add(bigFood);
                     }
                     else
                     {
@@ -254,25 +259,6 @@ namespace Pacman_Game
             DoCollisionCheck();
         }
 
-
-
-        //private Direction OppositeDirection(Direction direction)
-        //{
-        //    switch (direction)
-        //    {
-        //        case Direction.Left:
-        //            return Direction.Right;
-        //        case Direction.Right:
-        //            return Direction.Left;
-        //        case Direction.Up:
-        //            return Direction.Down;
-        //        case Direction.Down:
-        //            return Direction.Up;
-        //        default:
-        //            throw new ArgumentException("Invalid direction.");
-        //    }
-        //}
-
         private void gameTimer_Tick(object sender, EventArgs e)
         {
             MovePacmanImage();
@@ -343,21 +329,30 @@ namespace Pacman_Game
 
         private void DoCollisionCheck()
         {
+            CheckFruitCollision();
+            CheckFoodCollision();
+            CheckBigFoodCollision();
+            CheckGhostCollision();
+            CheckOutOfBounds();
+        }
+
+        private void CheckFruitCollision()
+        {
             if (fruit == null)
             {
                 if (currFoodCount == totalFoodCount / 2)
                 {
                     DrawFruit();
                 }
-                
             }
             else if ((PacmanImage.Position.X == Canvas.GetLeft(fruit)) && (PacmanImage.Position.Y == Canvas.GetTop(fruit)))
             {
                 EatFruit();
             }
-            
+        }
 
-            // Snake eats small red
+        private void CheckFoodCollision()
+        {
             Ellipse collidedFood = null;
             foreach (UIElement food in GameArea.Children)
             {
@@ -373,59 +368,62 @@ namespace Pacman_Game
                     }
                 }
             }
-
             if (collidedFood != null)
             {
                 EatFood(collidedFood);
             }
+        }
 
-            // Snake eats separate food
-            UIElement collidedSeparateFood = null;
-            foreach (UIElement separateFood in separateFoods)
+        private void CheckBigFoodCollision()
+        {
+            UIElement collidedBigFood = null;
+            foreach (UIElement bigFood in bigFoods)
             {
-                double left = Canvas.GetLeft(separateFood) - squareSize * 0.25;
-                double top = Canvas.GetTop(separateFood) - squareSize * 0.25;
+                double left = Canvas.GetLeft(bigFood) - squareSize * 0.25;
+                double top = Canvas.GetTop(bigFood) - squareSize * 0.25;
 
                 if ((PacmanImage.Position.X == left) && (PacmanImage.Position.Y == top))
                 {
-                    collidedSeparateFood = separateFood;
+                    collidedBigFood = bigFood;
                     break;
                 }
             }
-
-            if (collidedSeparateFood != null)
+            if (collidedBigFood != null)
             {
-                EatSeparateFood(collidedSeparateFood);
+                EatBigFood(collidedBigFood);
             }
             if (currFoodCount == 0)
             {
                 FinishGame();
             }
 
+        }
 
-            // Check if the snake collided with any of the ghosts
-            double ghostCollision = CheckGhostCollision();
-            
+        private void CheckGhostCollision()
+        {
+            double ghostCollision = IsGhostCollision();
+
             switch (ghostCollision)
             {
                 case 0:
                     break;
                 case 1:
-                    HandleGhostCollision(Ghost1);
+                    if (HandleGhostCollision(Ghost1)) isGhost1 = false;
                     break;
                 case 2:
-                    HandleGhostCollision(Ghost2);
+                    if (HandleGhostCollision(Ghost2)) isGhost2 = false;
                     break;
                 case 3:
-                    HandleGhostCollision(Ghost3);
+                    if (HandleGhostCollision(Ghost3)) isGhost3 = false;
                     break;
                 case 4:
-                    HandleGhostCollision(Ghost4);
+                    if (HandleGhostCollision(Ghost4)) isGhost4 = false;
                     break;
             }
+        }
 
-
-            // Snake out of bounds
+        private void CheckOutOfBounds()
+        {
             if ((PacmanImage.Position.Y < 0) || (PacmanImage.Position.Y >= GameArea.ActualHeight) ||
                 (PacmanImage.Position.X < 0) || (PacmanImage.Position.X >= GameArea.ActualWidth))
             {
@@ -441,20 +439,31 @@ namespace Pacman_Game
             }
         }
 
-        private void HandleGhostCollision(Path ghost)
+        private Point GetElementPosition(UIElement element)
+        {
+            double left = Canvas.GetLeft(element);
+            double top = Canvas.GetTop(element);
+            return new Point(left, top);
+        }
+
+
+        private bool HandleGhostCollision(Path ghost)
         {
             SolidColorBrush blueBrush = new SolidColorBrush(Colors.Blue);
             SolidColorBrush ghostFillBrush = ghost.Fill as SolidColorBrush;
             if (ghostFillBrush != null && ghostFillBrush.Color == blueBrush.Color)
             {
+                currentScore += 200;
                 GameArea.Children.Remove(ghost);
+                return true;
             }
             else
             {
                 LoseLife();
+                return false;
             }
         }
-        private double CheckGhostCollision()
+        private double IsGhostCollision()
         {
             double pacmanX = PacmanImage.Position.X;
             double pacmanY = PacmanImage.Position.Y;
@@ -471,19 +480,19 @@ namespace Pacman_Game
             double ghost4X = Canvas.GetLeft(Ghost4);
             double ghost4Y = Canvas.GetTop(Ghost4);
 
-            if (pacmanX == ghost1X && pacmanY == ghost1Y)
+            if (pacmanX == ghost1X && pacmanY == ghost1Y && isGhost1)
             {
                 return 1;
             }
-            else if (pacmanX == ghost2X && pacmanY == ghost2Y)
+            else if (pacmanX == ghost2X && pacmanY == ghost2Y && isGhost2)
             {
                 return 2;
             }
-            else if (pacmanX == ghost3X && pacmanY == ghost3Y)
+            else if (pacmanX == ghost3X && pacmanY == ghost3Y && isGhost3)
             {
                 return 3;
             }
-            else if (pacmanX == ghost4X && pacmanY == ghost4Y)
+            else if (pacmanX == ghost4X && pacmanY == ghost4Y && isGhost4)
             {
                 return 4;
             }
@@ -499,19 +508,8 @@ namespace Pacman_Game
             remainingLives--;
             if (remainingLives > 0)
             {
-                //// Remove the previous pacmanPath element from the GameArea
-                //GameArea.Children.Remove(PacmanImage.UiElement);
-
-                //// Create a new pacmanPath and add it to the GameArea
-                //pacmanPath = DrawPacmanImage.DrawPacmanImageRight();
-                //pacmanCanvas.Children.Clear();
-                //pacmanCanvas.Children.Add(pacmanPath);
-                //PacmanImage.UiElement = pacmanCanvas;
-                //GameArea.Children.Add(PacmanImage.UiElement);
                 ResetPacman(DrawPacmanImage.DrawPacmanImageRight());
-                // Restart
                 gameTimer.IsEnabled = false;
-                ghostTimer.IsEnabled = false;
                 MessageBox.Show($"You died! But you still have {remainingLives} lives remaining.", "Pacman");
                 RestartGame();
             }
@@ -524,7 +522,6 @@ namespace Pacman_Game
         private void ResetPacman(Path pacmanPath)
         {
             GameArea.Children.Remove(PacmanImage.UiElement);
-            //pacmanImage = DrawPacmanImage.DrawPacmanImageRight()
             // Create a new pacmanPath and add it to the GameArea
             pacmanCanvas.Children.Clear();
             pacmanCanvas.Children.Add(pacmanPath);
@@ -564,11 +561,11 @@ namespace Pacman_Game
             GameArea.Children.Remove(ellipse);
             UpdateGameStatus();
         }
-        private void EatSeparateFood(UIElement separateFood)
+        private void EatBigFood(UIElement bigFood)
         {
             currentScore += 50;
-            GameArea.Children.Remove(separateFood);
-            separateFoods.Remove(separateFood);
+            GameArea.Children.Remove(bigFood);
+            bigFoods.Remove(bigFood);
             UpdateGameStatus();            
 
             // Create a timer to call ToggleGhostColor() after eight seconds
@@ -608,6 +605,7 @@ namespace Pacman_Game
         }
         private void ResetValues()
         {
+            GameArea.Children.Remove(fruit);
             currentScore = 0;
             remainingLives = 3;
             DrawGameArea();
